d4f714d65fda17436b752980f30b45bdbf43f0ad
Davide <kimhak300@gmail.com>
2024-05-09 19:31:13 +0700

    call function getDashboardInfo in dashboard.component

--- src/app/main/dashboard/dashboard.component.ts
+++ src/app/main/dashboard/dashboard.component.ts
@@ -1,4 +1,3 @@
-
 // ==============================================>> Core Library
 import { Component, OnInit } from '@angular/core';
 
@@ -10,63 +9,51 @@ import { SnackbarService } from 'app/shared/services/snackbar.service';
 import { DashboardService } from './dashboard.service';
 
 @Component({
-
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
-
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
-
 export class DashboardComponent implements OnInit {
+    // ===>> Public Variables used in the component and template.
+    public totalSaleToday: number = 0; // For Displaying Total Price.
+    public isLoading: boolean = false; // for loading Spinner UI Status
 
-  // ===>> Public Variables used in the component and template.
-  public totalSaleToday: number = 0; // For Displaying Total Price.
-  public isLoading: boolean     = false; // for loading Spinner UI Status
+    constructor(
+        //===> Private Variables used in this component only
+        private _dashboardService: DashboardService, // for API calling
+        private _snackBar: SnackbarService // for Displaying Message
+    ) {}
 
-  constructor(
-    //===> Private Variables used in this component only
-    private _dashboardService: DashboardService, // for API calling
-    private _snackBar: SnackbarService // for Displaying Message
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
+    ngOnInit(): void {
+        //===> Call API for Dashobard Data.
+        this.getDashboardInfo();
+    }
 
-  ) {
+    //=======================================================>> Function getDashobardInfo in this component
+    getDashboardInfo(): any {
+        //===> Call API for Dashobard Data in Service.
+        this._dashboardService.getDashboardInfo().subscribe(
+            (res: any) =>
+                // ===================================================================>> Success: HTTP 200
+                {
+                    //===> Hide Loading Spinner UI
+                    this.isLoading = false;
 
-  }
+                    //===> Update Total Price by Today.
+                    this.totalSaleToday = res.total_sale_today;
 
+                    // ===================================================================>> Not Success
+                },
+            (err: any) => {
+                //===> Hide Loading Spinner UI
+                this.isLoading = false;
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
-  ngOnInit(): void {
-
-    //===> Call API for Dashobard Data.
-
-
-  }
-
-  //=======================================================>> Function getDashobardInfo in this component
-  getDashboardInfo(): any {
-
-    //===> Call API for Dashobard Data in Service.
-    this._dashboardService.getDashboardInfo().subscribe(( res: any) =>
-    // ===================================================================>> Success: HTTP 200
-    {
-
-      //===> Hide Loading Spinner UI
-      this.isLoading = false;
-
-      //===> Update Total Price by Today.
-      this.totalSaleToday = res.total_sale_today;
-
-    // ===================================================================>> Not Success
-    }, (err: any) => {
-
-      //===> Hide Loading Spinner UI
-      this.isLoading = false;
-
-      //===> Display Error Message
-      this._snackBar.openSnackBar('Something went wrong.', 'error');
-
-    });
-  }
-
+                //===> Display Error Message
+                this._snackBar.openSnackBar('Something went wrong.', 'error');
+            }
+        );
+    }
 }
