06fac2e5fe995b7975ba4854dd2bf4058e877af8
Davide <kimhak300@gmail.com>
2024-05-16 00:31:45 +0700

    Create listing component

---
+++ src/app/main/my-profile/listing/listing.component.ts
@@ -0,0 +1,13 @@
+// ==========================================================>> Core Library
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-listing',
+    templateUrl: './listing.component.html',
+    styleUrls: ['./listing.component.scss'],
+})
+export class ListingComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
