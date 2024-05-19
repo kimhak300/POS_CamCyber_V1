8aa74a7308936a8a80e4003463ced79130b024a0
Davide <kimhak300@gmail.com>
2024-05-19 11:55:35 +0700

    fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-// =================================================>> Core Library
+// ============================================================================>> Core Library
 use Illuminate\Http\Response; // For Responsing data back to Client
 
 // ============================================================================>> Custom Library
@@ -11,29 +11,24 @@ use App\Http\Controllers\MainController;
 
 // Model
 
+use App\Models\Order\Order;
+
+
 
 class DashboardController extends MainController
 {
-
-    public function getDashboardInfo(){
-
-        // ===>> Get order data from DB and sum total_price using Function Sum
-        $totalSaleToday = Order::sum('total_price');
-
-        // ===>> Prepare response format
-        $data = [
-            'total_sale_today'      => $totalSaleToday
-        ];
-
-        // ===>> Success Response Back to Client
-
     public function getDashboardInfo()
     {
-        $totalSaleToday = 0;
+       //Get order data from db and sum total_price using funtion sum
+       $totalSaleToday = Order::sum('total_price');
+       //Prepare respone
+       $data =[
+           'total_sale_today'  => $totalSaleToday,
+       ];
 
 
-        return response()->json($data, Response::HTTP_OK);
+       // respone to client
+       return response()->json($data, Response::HTTP_OK);
 
     }
-}
 }
