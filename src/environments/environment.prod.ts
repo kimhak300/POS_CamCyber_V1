154a978b56ecc87cdd8bf6d43f8d1205c1de9228
Davide <kimhak300@gmail.com>
2024-05-28 17:15:55 +0700

    web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
-
     production: true,
-    API_BASE_URL      : 'http://localhost:8002/api',
-    FILE_PUBLIC_BASE_URL     : 'http://localhost:8003/'
-
+    API_BASE_URL: 'https://api.kimhak.cloud/api',
+    FILE_PUBLIC_BASE_URL: 'https://file.kimhak.cloud/',
 };
